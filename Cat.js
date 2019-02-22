9744da941a8ce95dfb176c2dc3f14397e50a7e55
ethanwc <ethanwc@u.washington.edu>
2019-02-21 23:45:43 -0800

    added a cat

--- Cat.js
+++ Cat.js
@@ -1,6 +1,6 @@
 class Cat {
     constructor() {
-        this.animation = new Animation(AM.getAsset("./img/cat.png"), 0, 0, 48, 42, 3, .1, 1, true, false);
+        this.animation = new Animation(AM.getAsset("./img/cat.png"), 0, 0, 48, 42, 3, .1, 3, true, false);
         this.x = 200;
         this.y = 200;
     }
@@ -10,6 +10,6 @@ class Cat {
     }
 
     draw(ctx) {
-        this.animation.drawFrame(gameEngine.clockTick, ctx, this.x, this.y, 1);
+        this.animation.drawFrame(gameEngine.clockTick, ctx, this.x, this.y, 3);
     }
 }
--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let AM = new AssetManager();
+AM.queueDownload("./img/cat.png");
 let gameEngine = undefined;
 
 AM.downloadAll(function () {
     let canvas = document.getElementById("gameWorld");
     let ctx = canvas.getContext("2d");
-
+    let width = screen.width * .6;
 
     document.getElementById("gameWorld").width = width;
     // noinspection JSSuspiciousNameCombination
